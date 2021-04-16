1bcd05d063c78a0891a555f64c83ba04a1ce081d
Danilo-Zekovic <danilozeka93@gmail.com>
2021-04-16 06:57:48 +0200

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from '../components/header'
+import Layout from '../components/Layout'
+
+export default () => {
+  return (
+    <Layout>
+      <Header headerText="ZEMO Page Not Found" />
+      <div>ZEMO apologizes for this inconvenience. But on the other hand what were you thinking? Trying to access an unexisting page :eye-roll:</div>
+    </Layout>
+  )
+}
